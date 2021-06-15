1cf69d80e853f6710ffda4b0c07c864bf78354d9
Jhonata <gutemberg.jhonata@gmail.com>
2021-06-15 07:28:05 -0300

    Create user data form in Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,54 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ShapeCademy</title>
+        <title>GymStation</title>
       </Head>
 
       <main className={styles.mainContainer}>
-        <h1>Hello World!</h1>
+        <form>
+          <label htmlFor="gender">Gênero</label>
+          <div id="gender">
+            <button type="button" className={styles.selected}>Feminino</button>
+            <button type="button">Masculino</button>
+          </div>
+          <label htmlFor="age">Idade</label>
+          <div id="age">
+            <input type="number" placeholder="35" />
+            <span>35</span>
+          </div>
+          <label htmlFor="height">Altura</label>
+          <div id="height">
+            <input type="number" placeholder="1.60" />
+            <span>m</span>
+          </div>
+          <label htmlFor="weight">Peso</label>
+          <div id="weight">
+            <input type="number" placeholder="62.5" />
+            <span>m</span>
+          </div>
+          <label htmlFor="waist">Circunferência da cintura</label>
+          <div id="waist">
+            <input type="number" placeholder="120" />
+            <span>cm</span>
+          </div>
+          <label htmlFor="neck">Circunferência do pescoço</label>
+          <div id="neck">
+            <input type="number" placeholder="120" />
+            <span>cm</span>
+          </div>
+          <label htmlFor="hip">Circunferência do quadril</label>
+          <div id="hip">
+            <input type="number" placeholder="120" />
+            <span>cm</span>
+          </div>
+          <label htmlFor="gender">Atividade física</label>
+          <div id="gender">
+            <button type="button">Sedentária</button>
+            <button type="button" className={styles.selected}>Moderada</button>
+            <button type="button">Intensa</button>
+          </div>
+          <button type="submit">Calcular</button>
+        </form>
       </main>
     </>
   );
