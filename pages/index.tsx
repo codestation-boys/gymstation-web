99a08e301d20b1d15e0e88680b340f8da295917b
Jhonata <gutemberg.jhonata@gmail.com>
2021-06-15 07:38:20 -0300

    Create user data form states

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import Head from "next/head";
+import { useState } from "react";
 
 import styles from "./home.module.scss";
 
 export default function Home() {
+  const [gender, setGender] = useState('female');
+  const [age, setAge] = useState();
+  const [height, setHeight] = useState();
+  const [weight, setWeight] = useState();
+  const [waist, setWaist] = useState();
+  const [neck, setNeck] = useState();
+  const [hip, setHip] = useState();
+  const [activity, setActivity] = useState('regular');
+
   return (
     <>
       <Head>
@@ -13,8 +23,8 @@ export default function Home() {
         <form>
           <label htmlFor="gender">Gênero</label>
           <div id="gender">
-            <button type="button" className={styles.selected}>Feminino</button>
-            <button type="button">Masculino</button>
+            <button type="button" name="female" className={styles.selected}>Feminino</button>
+            <button type="button" name="male">Masculino</button>
           </div>
           <label htmlFor="age">Idade</label>
           <div id="age">
@@ -46,10 +56,10 @@ export default function Home() {
             <input type="number" placeholder="120" />
             <span>cm</span>
           </div>
-          <label htmlFor="gender">Atividade física</label>
-          <div id="gender">
+          <label htmlFor="activity">Atividade física</label>
+          <div id="activity">
             <button type="button">Sedentária</button>
-            <button type="button" className={styles.selected}>Moderada</button>
+            <button type="button" name="regular" className={styles.selected}>Moderada</button>
             <button type="button">Intensa</button>
           </div>
           <button type="submit">Calcular</button>
