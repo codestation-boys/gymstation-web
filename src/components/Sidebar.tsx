f29df1fe3648642a90793a98456b3757a1fc974b
savio591 <savio591@hotmail.com>
2021-06-17 17:49:23 -0300

    fixed sidebar styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,45 +1,31 @@
-import {Avatar, Box, Flex, HStack, Icon, Image, Link, Stack, Text} from "@chakra-ui/react";
+import {Avatar, Box, Flex, Icon, Image, Link, Stack, Text, Divider,} from "@chakra-ui/react";
 import {RiDashboardLine, RiDossierLine, RiNewspaperLine} from "react-icons/ri";
 import {FaDumbbell} from "react-icons/fa";
 
 export function Sidebar() {
   return (
-    <Box as='aside' w='64' mr='8'>
+    <Box as='aside' w='64' m='2.5rem' display={['none', 'none', 'block' ]}>
       <Stack spacing='12'>
-        <Flex
-          spacing='4'
-          align='center'
-        >
-          <Box boxSize="16" mr='4'>
-            <Image src="/images/logo.svg" alt="GYMSTATION"/>
-          </Box>
-          <Box width="32">
-            <Image src="/images/logo_label.svg" alt="GYMSTATION"/>
-          </Box>
-        </Flex>
-        <Flex
-          align='center'
-        >
-          <Avatar mr='4' size='md' name='Enos Andrade' src='https://github.com/enosads.png'/>
-          <Box>
-            <Text>Enos Andrade</Text>
-            <Text color='gray.300' fontSize='small'>
-              enosads@gmail.com
-            </Text>
-          </Box>
-        </Flex>
+        <Box boxSize='16' width='218px' height='48px'>
+          <Image src='/images/logo_row_wide.svg' alt='Gymstation logo' />
+        </Box>
+
         <Box>
           <Text fontWeight='bold' color='gray.400' fontSize='small'>
             GERAL
           </Text>
           <Stack spacing='4' mt='8' align='stretch'>
             <Link display='flex' align='center' href='/dashboard'>
-              <Icon as={RiDashboardLine} fontSize='20'/>
-              <Text ml='4' fontWeight='medium'>Dashboard</Text>
+              <Icon as={RiDashboardLine} fontSize='20' />
+              <Text ml='4' fontWeight='medium'>
+                Dashboard
+              </Text>
             </Link>
             <Link display='flex' align='center' href='/blog'>
-              <Icon as={RiNewspaperLine} fontSize='20'/>
-              <Text ml='4' fontWeight='medium'>Blog</Text>
+              <Icon as={RiNewspaperLine} fontSize='20' />
+              <Text ml='4' fontWeight='medium'>
+                Blog
+              </Text>
             </Link>
           </Stack>
         </Box>
@@ -49,14 +35,32 @@ export function Sidebar() {
           </Text>
           <Stack spacing='4' mt='8' align='stretch'>
             <Link display='flex' align='center'>
-              <Icon as={RiDossierLine} fontSize='20'/>
-              <Text ml='4' fontWeight='medium'>Medições</Text>
+              <Icon as={RiDossierLine} fontSize='20' />
+              <Text ml='4' fontWeight='medium'>
+                Medições
+              </Text>
             </Link>
             <Link display='flex' align='center'>
-              <Icon as={FaDumbbell} fontSize='20'/>
-              <Text ml='4' fontWeight='medium'>Treino</Text>
+              <Icon as={FaDumbbell} fontSize='20' />
+              <Text ml='4' fontWeight='medium'>
+                Treino
+              </Text>
             </Link>
           </Stack>
+        </Box>
+        <Divider />
+        <Box>
+          <Flex direction='row' spacing='2'>
+            <Avatar
+              size='md'
+              name='Savio Castelo'
+              src='https://github.com/savio591.png'
+            />
+            <Box ml='2'>
+              <Text fontSize='lg' fontWeight='medium'>Savio Castelo</Text>
+              <Text fontSize='sm' fontWeight='normal'>savio591@hotmail.com</Text>
+            </Box>
+          </Flex>
         </Box>
       </Stack>
     </Box>
