38782d24f35571895eac6becf11c708aa4d516d8
savio591 <savio591@hotmail.com>
2021-06-18 00:17:34 -0300

    added Login form hook states and SignIn handler with next-auth

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,46 +1,53 @@
-import {Button, Flex, Stack} from "@chakra-ui/react";
-import {Input} from "../components/Form/Input";
-
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { signIn } from "next-auth/client";
+import { FormEvent, useState } from "react";
+import { Input } from "../components/Form/Input";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn(event: FormEvent) {
+    event.preventDefault();
+
+    signIn("credentials", { redirect: false, email, password });
+
+    setEmail("");
+    setPassword("");
+  }
+
   return (
-    <Flex
-      width='100vw'
-      height='100vh'
-      align='center'
-      justify='center'
-    >
+    <Flex width="100vw" height="100vh" align="center" justify="center">
       <Flex
-        as='form'
-        width='100%'
+        as="form"
+        width="100%"
         maxWidth={360}
-        backgroundColor='gray.800'
-        padding='8'
-        borderRadius='8'
-        flexDirection='column'
+        backgroundColor="gray.800"
+        padding="8"
+        borderRadius="8"
+        flexDirection="column"
+        onSubmit={handleSignIn}
       >
-        <Stack spacing='4'>
+        <Stack spacing="4">
           <Input
-            type='email'
-            name='email'
-            label='E-mail'
+            type="email"
+            name="email"
+            label="E-mail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
           />
           <Input
-            type='password'
-            name='password'
-            label='Senha'
+            type="password"
+            name="password"
+            label="Senha"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
           />
         </Stack>
-        <Button
-          type='submit'
-          marginTop='6'
-          colorScheme='red'
-          size='lg'
-        >
+        <Button type="submit" marginTop="6" colorScheme="red" size="lg">
           Entrar
         </Button>
-
       </Flex>
     </Flex>
-  )
+  );
 }
