020fa48b413c4fe70ccf3fc0743ad88a0b6b17f3
savio591 <savio591@hotmail.com>
2021-06-18 16:01:43 -0300

    changed static dashboard page to serverSideRendering

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/dashboard',
+        source: "/",
+        destination: "/login",
         permanent: false,
       },
-    ]
+    ];
   },
-}
+};
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,98 +1,150 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar";
 import dynamic from "next/dynamic";
+import { getSession, useSession } from "next-auth/client";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
+import { GetServerSideProps } from "next";
 
-const Chart = dynamic(() => import('react-apexcharts'), {
-  ssr: false
-})
+// type UserData = {
+//   name: string;
+//   email: string;
+//   image: string;
+// };
+
+// interface DashboardProps {
+//   userData: UserData;
+// }
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 const options = {
   colors: [theme.colors.red[500]],
   chart: {
     toolbar: {
-      show: false
+      show: false,
     },
     zoom: {
-      enabled: false
+      enabled: false,
     },
-    foreColor: theme.colors.gray[500]
+    foreColor: theme.colors.gray[500],
   },
   grid: {
-    show: false
+    show: false,
   },
   dataLabels: {
-    enabled: false
+    enabled: false,
   },
   tooltip: {
-    enabled: false
+    enabled: false,
   },
   xAxis: {
-    type: 'datetime',
+    type: "datetime",
     axisBorder: {
-      color: theme.colors.gray[600]
+      color: theme.colors.gray[600],
     },
     axisTicks: {
-      color: theme.colors.gray[600]
+      color: theme.colors.gray[600],
     },
     categories: [
-      '2021-03-18T00:00:00.000Z',
-      '2021-03-19T00:00:00.000Z',
-      '2021-03-20T00:00:00.000Z',
-      '2021-03-21T00:00:00.000Z',
-      '2021-03-22T00:00:00.000Z',
-      '2021-03-23T00:00:00.000Z',
-      '2021-03-24T00:00:00.000Z',
-    ]
+      "2021-03-18T00:00:00.000Z",
+      "2021-03-19T00:00:00.000Z",
+      "2021-03-20T00:00:00.000Z",
+      "2021-03-21T00:00:00.000Z",
+      "2021-03-22T00:00:00.000Z",
+      "2021-03-23T00:00:00.000Z",
+      "2021-03-24T00:00:00.000Z",
+    ],
   },
   fill: {
     colors: [theme.colors.red[500]],
     opacity: 0.3,
-    type: 'gradient',
+    type: "gradient",
     gradient: {
-      shade: 'dark',
+      shade: "dark",
       opacityFrom: 0.7,
-      opacityTo: 0.3
-    }
-  }
-}
+      opacityTo: 0.3,
+    },
+  },
+};
 
 const series = [
   {
-    name: 'series1',
+    name: "series1",
     data: [31, 120, 10, 28, 51, 18, 109],
-  }
-]
+  },
+];
 
 export default function Dashboard() {
+  const [session] = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session) {
+      router.push(`/login`);
+    }
+  }, [session]);
+
   return (
-    <Flex w='100%'>
+    <Flex w="100%">
       <Sidebar />
-      <SimpleGrid flex='1' gap='4' minChildWidth='320px' align='flex-start' p='8'>
-        <Box p='8' bg='gray.800' borderRadius={8}>
-          <Text fontSize='lg' mb='4'>
+      <SimpleGrid
+        flex="1"
+        gap="4"
+        minChildWidth="320px"
+        align="flex-start"
+        p="8"
+      >
+        <Box p="8" bg="gray.800" borderRadius={8}>
+          <Text fontSize="lg" mb="4">
             Peso
           </Text>
-          <Chart options={options} series={series} type='area' height={160} />
+          <Chart options={options} series={series} type="area" height={160} />
         </Box>
-        <Box p='8' bg='gray.800' borderRadius={8} PB='4'>
-          <Text fontSize='lg' mb='4'>
+        <Box p="8" bg="gray.800" borderRadius={8} PB="4">
+          <Text fontSize="lg" mb="4">
             Cintura
           </Text>
-          <Chart options={options} series={series} type='area' height={160} />
+          <Chart options={options} series={series} type="area" height={160} />
         </Box>
-        <Box p='8' bg='gray.800' borderRadius={8} PB='4'>
-          <Text fontSize='lg' mb='4'>
+        <Box p="8" bg="gray.800" borderRadius={8} PB="4">
+          <Text fontSize="lg" mb="4">
             Pescoço
           </Text>
-          <Chart options={options} series={series} type='area' height={160} />
+          <Chart options={options} series={series} type="area" height={160} />
         </Box>
-        <Box p='8' bg='gray.800' borderRadius={8} PB='4'>
-          <Text fontSize='lg' mb='4'>
+        <Box p="8" bg="gray.800" borderRadius={8} PB="4">
+          <Text fontSize="lg" mb="4">
             Quadril
           </Text>
-          <Chart options={options} series={series} type='area' height={160} />
+          <Chart options={options} series={series} type="area" height={160} />
         </Box>
       </SimpleGrid>
     </Flex>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  console.log({ params });
+  const session = await getSession({ req });
+  console.log({session})
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      userData: session.user,
+    },
+  };
+};
